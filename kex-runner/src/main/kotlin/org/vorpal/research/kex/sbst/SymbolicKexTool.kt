392c5c63527e8a4c4989fc1705598debabc26494
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2023-06-29 12:38:34 +0300

    SymbolicKexTool: generate tests only for non-private methods

--- kex-runner/src/main/kotlin/org/vorpal/research/kex/sbst/SymbolicKexTool.kt
+++ kex-runner/src/main/kotlin/org/vorpal/research/kex/sbst/SymbolicKexTool.kt
@@ -129,7 +129,7 @@ class SymbolicKexTool : Tool {
         val klass = context.cm[canonicalName]
         log.debug("Running on klass $klass")
         try {
-            InstructionSymbolicCheckerGraph.run(context, klass.allMethods)
+            InstructionSymbolicCheckerGraph.run(context, klass.allMethods.filter { !it.isPrivate }.toSet())
         } catch (e: Throwable) {
             log.error("Error: ", e)
         }
